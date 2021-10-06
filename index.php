fcf382c6d7bc4867e76699a67b932c7d91f7489c
m1nn1man <maxecohen2001@gmail.com>
2021-10-06 12:31:16 -0400

    favicon part 2

--- index.php
+++ index.php
@@ -95,7 +95,8 @@ button {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Spotifeed | Your Spotify Feed</title>
     <link rel="stylesheet" href="./style.css" />
-    <link rel="stylesheet" href="./style.css" />
+    <link rel="shortcut icon" type="image/jpg" href="assets/favicon.ico">
+    
     <link rel="preconnect" href="https://fonts.googleapis.com"> 
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
