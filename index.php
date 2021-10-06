8bece70c243a6ded23f2934fb3b97c9e3cfa4f5c
m1nn1man <maxecohen2001@gmail.com>
2021-10-06 11:49:52 -0400

    updating login page to match style

--- index.php
+++ index.php
@@ -85,30 +85,34 @@ function checkAdmin( $name ) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Group 7 Portal - CS490</title>
+    <title>Spotifeed | Your Spotify Feed</title>
     <link rel="stylesheet" href="./style.css" />
+    <link rel="stylesheet" href="./style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 
-<footer class="footer2" style="width: 100%; bottom: 0; height: 20vh; background-color: #2f3a41; text-align: center">
-    <img src="assets/logo_NJIT.png" class="imgFitMid" style="height: 17vh; margin-left: 50%; transform: translate(-50%, 0.75vh);" />
+<footer class="footer2" style="width: 100%; bottom: 0; height: 20vh; background-color: #161616; text-align: center; font-family: 'Montserrat', sans-serif;">
+    <img src="assets/logo_spotify.png" class="imgFitMid" style="height: 17vh; margin-left: 50%; transform: translate(-50%, 0.75vh);" />
 </footer>
 
-<body style="background-color: #2f3a41">
-    <div class="col-12 bodyText" style="font-size: max(3vw, 3.5vh); background-color: #2f3a41; text-align: center; padding-top: 5vh; overflow: visible; color: #e8e8e8;">
-        Group 7 Portal
+<body style="background-color: #161616; font-family: 'Montserrat', sans-serif;">
+    <div class="col-12 bodyText" style="font-size: max(3vw, 3.5vh); background-color: #161616; text-align: center; padding-top: 5vh; overflow: visible; color: #e8e8e8;">
+        Spotifeed
     </div>
-    <div class="col-12" style="height: 50vh; padding-top: 15vh; background-color: #2f3a41; text-align: center">
+    <div class="col-12" style="height: 50vh; padding-top: 15vh; background-color: #161616; text-align: center">
         <form method="POST">
             <div class="col-6 push-3 colsm-10 pushsm-1">
-                <div class="col-6 colsm-12">
-                    <input type="text" name="username_input" placeholder="Username" value="<?php echo $_SESSION['username']; ?>" style="width: 100%; margin: 2px 0; background-color: #32424b; border-color: #597687; border-style: solid; color: #e8e8e8; padding: 1vh 1vw; border-radius: 1vh; font-size: max(2vw, 2vh)" required />
+                <div class="col-12" style="margin-bottom: 1ch">
+                    <input type="text" name="username_input" placeholder="Username" value="<?php echo $_SESSION['username']; ?>" style="width: 64%; margin: 2px 0; background-color: #000; border-color: #56b35e; border-style: solid; color: #fff; padding: 1vh 1vw; border-radius: 1vh; font-size: max(2vw, 2vh); text-align: center;" required />
                 </div>
-                <div class="col-6 colsm-12">
-                    <input type="password" name="password_input" placeholder="Password" value="<?php echo $_SESSION['password']; ?>" style="width: 100%; margin: 2px 0; background-color: #32424b; border-color: #597687; border-style: solid; color: #e8e8e8; padding: 1vh 1vw; border-radius: 1vh; font-size: max(2vw, 2vh)" required />
+                <div class="col-12" style="margin-bottom: 2ch">
+                    <input type="password" name="password_input" placeholder="Password" value="<?php echo $_SESSION['password']; ?>" style="width: 64%; margin: 2px 0; background-color: #000; border-color: #56b35e; border-style: solid; color: #fff; padding: 1vh 1vw; border-radius: 1vh; font-size: max(2vw, 2vh); text-align: center;" required />
                 </div>
             </div>
             <div class="col-12">
-                <button type="submit" name="btn_login_update" style="width: 30%; margin: 2px 0; background-color: #32424b; border-color: #597687; border-style: solid; color: #e8e8e8; padding: 1vh 1vw; border-radius: 1vh; font-size: max(2vw, 2vh)"><strong>Login</strong></button>
+                <button type="submit" name="btn_login_update" style="width: 32%; margin: 2px 0; background-color: #56b35e; border-color: #56b35e; border-style: outset; color: #000; padding: 1vh 1vw; border-radius: 1vh; font-size: max(2vw, 2vh)"><strong>Login</strong></button>
             </div>
         </form>
     </div>
