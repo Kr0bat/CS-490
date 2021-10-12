030824a266c60452984cd19d087150fa0b78519c
m1nn1man <maxecohen2001@gmail.com>
2021-10-12 18:56:50 -0400

    new post page with input boxes

--- newPost.php
+++ newPost.php
@@ -12,6 +12,14 @@ input {
 button {
     cursor: pointer;
 }
+
+.newPostBackBox {
+    border-radius: 2.5ch;
+    background: #1b1b1bff;
+    border-color: #373737;
+    border-width: 0.35ch;
+    border-style: solid;
+}
 </style>
 
 <head>
@@ -28,10 +36,28 @@ button {
 
 <body style="background-color: #161616; font-family: 'Montserrat', sans-serif;">
     <div class="col-12 subtitleBold" style="text-align: left; margin: max(1ch, 2vh) 0 0 max(2ch, 2vw)">
-        <a href="/~kg448/feed.php" class="subtitleBold" style="text-decoration: none;">Cancel</a>
+        <a href="/~kg448/feed.php" class="subtitleBold" style="text-decoration: none; font-size: 18px">Cancel</a>
     </div>
-    <div class="col-12 bodyText" style="font-size: clamp(1rem, max(5vh, 5vw), 4rem); background-color: #161616; text-align: center; padding-top: 5vh; overflow: visible; color: #e8e8e8;">
+    <div class="col-12 titleBold" style="text-align: center; margin-top: 3ch;">
         New Post
+    </div>
+    <div class="col-8 push-2 colsm-12 pushsm-0 bodyLight newPostBackBox" style="text-align: left; margin-top: 3ch;">
+        <form method="POST">
+            <div class="col-10 push-1" style="margin: 2ch 1ch 0.5ch 1ch;">
+                <div class="col-55 colsm-10 pushsm-1">
+                    <input type="text" name="newpost_title" placeholder="Title of your post" value="" style="width: 100%; background-color: #000; border-color: #56b35e; border-style: solid; color: #fff; padding: 1vh 1vw; border-radius: 0.75ch; font-size: 20px;" required />
+                </div>
+                <div class="col-55 push-1 colsm-10 pushsm-1">
+                    <input type="text" name="newpost_link" placeholder="Paste Spotify song link here" value="" style="width: 100%; background-color: #000; border-color: #56b35e; border-style: solid; color: #fff; padding: 1vh 1vw; border-radius: 0.75ch; font-size: 20px;" required />
+                </div>
+            </div>
+            <div class="col-10 push-1" style="margin: 1ch 1ch 2ch 1ch;">
+                <input maxlength="280" type="text" name="newpost_description" placeholder="Add your own description here" value="" style="width: 100%; background-color: #000; border-color: #56b35e; border-style: solid; color: #fff; padding: 1vh 1vw; border-radius: 0.75ch; font-size: 20px; word-break: break-word; height: 7.5ch; vertical-align: top;" required />
+            </div>
+            <div class="col-10 push-1" style="margin: 0.5ch 1ch 2ch 1ch;">
+                <button type="submit" name="newpost_submit" style="width: 50%; background-color: #56b35e; border-color: #56b35e; border-style: outset; color: #000; border-radius: 0.75ch; font-size: 20px; margin-left: 50%; transform: translate(-50%, 0); padding: 0.5ch 0;"><strong>Post</strong></button>
+            </div>
+        </form>
     </div>
 </body>
 </html>
