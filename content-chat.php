66c02c3b96bbc401d6a83dde0215290dce280f6a
m1nn1man <maxecohen2001@gmail.com>
2021-10-17 13:20:24 -0400

    added button to make new chats

--- content-chat.php
+++ content-chat.php
@@ -26,9 +26,24 @@ session_start();
 <body>
     <div class="col-12" style="font-size: 22.5px; padding-left: 10ch">
         <div class="col-12" style="margin-top: 5vh">
-            <div class="col-10 push-1 titleBold" style="">
-                Direct Messages
-            </div>
+            <table class="col-10 push-1">
+                <tbody>
+                    <tr>
+                        <td>
+                            <div class="titleBold" style="">
+                                Direct Messages
+                            </div>
+                        </td>
+                        <td style="text-align: right;">
+                            <a href="" style="text-decoration: none;">
+                                <div class="subtitleLight" style="font-size: 20px; font-style: normal;">
+                                    + New Chat
+                                </div>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
         <div class="col-12" style="margin-top: 1vh">
             <div class="col-10 push-1 subtitleLight" style="font-size: 22.5px">
@@ -38,55 +53,88 @@ session_start();
         <div class="col-10 push-1" style="margin: 5vh 0">
         
             <?php
+
+
+            // $chatlist is updated by Middle End. Has two parameters: "last_msg" and "new".
             $chatlist = [
-                "Karim" => ["last_msg" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "new" => "true"], 
-                "Jose" => ["last_msg" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "new" => "true"],
-                "Mom" => ["last_msg" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "new" => "false"], 
-            ];
-            foreach($chatlist as $user => $content) {
+                "Karim" => ["last_msg" => "Lorem ipsum dolor sit amet", "new" => "true"], 
+                "Jose" => ["last_msg" => "Lorem ipsum dolor sit amet", "new" => "true"],
+                "Mom" => ["last_msg" => "Lorem ipsum dolor sit amet", "new" => "false"]];
+
+            $chatlist = [];
+
+            if (count($chatlist) == 0) {
+
                 print('
-                <a href="?chatWith='.$user.'">
-                    <div class="col-12 bodyBold dmContainer" style="margin: 0.5ch 0">
-                        <div class="col-12">
+                    <a href="">
+                        <div class="col-12 bodyBold dmContainer" style="margin: 0.5ch 0">
                             <div class="col-12">
-                                
-                                    <table>
-                                        <tbody>
-                                            <tr>
-                                                <td style="max-width: fit-content;">
-                                                    <span class="');
-                                                    
+                                <div class="col-12">
+                                    
+                                        <table>
+                                            <tbody>
+                                                <tr>
+                                                    <td style="padding-left: 0.35ch">
+                                                        + New Chat
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    ');
+
+            } else {
+                
+                foreach($chatlist as $user => $content) {
+                    print('
+                    <a href="?chatWith='.$user.'">
+                        <div class="col-12 bodyBold dmContainer" style="margin: 0.5ch 0">
+                            <div class="col-12">
+                                <div class="col-12">
+                                    
+                                        <table>
+                                            <tbody>
+                                                <tr>
+                                                    <td style="max-width: fit-content;">
+                                                        <span class="');
+                                                        
+                                                        if ($content['new'] == "true") {
+                                                            print('blueDot" style="margin-left: 1.5ch;"');
+                                                        } else {
+                                                            print('"');
+                                                        }
+
+                                                        print('>
+                                                            <img src="assets/profPic.jpeg" class="logoImg" style="border-width: 0.05px; border-radius: 100%; height: 1.53ch; border-style: solid; border-color: rgba(255, 255, 255, 0.15); margin-top: 0.4ch;" />
+                                                        </span>
+                                                    </td>
+                                                    <td style="padding-left: 0.35ch">
+                                                    ');
+                                                        
                                                     if ($content['new'] == "true") {
-                                                        print('blueDot" style="margin-left: 1.5ch;"');
+                                                        print('From ');
                                                     } else {
-                                                        print('"');
+                                                        print('');
                                                     }
 
-                                                    print('>
-                                                        <img src="assets/profPic.jpeg" class="logoImg" style="border-width: 0.05px; border-radius: 100%; height: 1.53ch; border-style: solid; border-color: rgba(255, 255, 255, 0.15); margin-top: 0.4ch;" />
-                                                    </span>
-                                                </td>
-                                                <td style="padding-left: 0.35ch">
-                                                ');
-                                                    
-                                                if ($content['new'] == "true") {
-                                                    print('From ');
-                                                } else {
-                                                    print('');
-                                                }
-
-                                                print(''.$user.'
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                
+                                                    print(''.$user.'
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    
+                                </div>
+                                <div class="col-12 subtitleLight" style="font-size: 18px; margin-top: 1.5ch; text-overflow: ellipsis; overflow: hidden; white-space: nowrap;">'.$content["last_msg"].'</div>
                             </div>
-                            <div class="col-12 subtitleLight" style="font-size: 18px; margin-top: 1.5ch; text-overflow: ellipsis; overflow: hidden; white-space: nowrap;">'.$content["last_msg"].'</div>
                         </div>
-                    </div>
-                </a>
-                ');
+                    </a>
+                    ');
+                }
+
             }
             ?>
 
