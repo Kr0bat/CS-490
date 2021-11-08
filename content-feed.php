633ac6dbf126dd6557a8e965cb8a5056a71bac63
m1nn1man <maxecohen2001@gmail.com>
2021-11-07 20:28:59 -0500

    testing getting likes to show on feed posts

--- content-feed.php
+++ content-feed.php
@@ -351,6 +351,9 @@ if (isset($_GET['successMsg'])) {
     <?php
 
         } else {
+            print('<pre>');
+            print_r($info);
+            print('</pre>');
 
     ?>
 
@@ -429,22 +432,23 @@ if (isset($_GET['successMsg'])) {
                                                 Artist
                                             </div>
                                         </div>
-                                        <div class="col-6" style="height: 8ch; overflow: hidden; text-overflow: ellipsis; word-break: break-word; padding-top: 5ch; text-align: right;">
+                                        <div class="col-6" style="height: 8ch; overflow: hidden; text-overflow: ellipsis; word-break: break-word; padding-top: 2ch; text-align: right;">
                                             <div class="col-12" style="">
                                                 <table style="width: 100%">
                                                     <tbody>
                                                         <tr>
-                                                            <td>
-                                                            </td>
-                                                            <td style="text-align: center">
-                                                                <span style="color: red; font-size: 30px; font-weight: bolder;"><?php echo $info['likeCount'] ?></span>
+                                                            <td style="width: 100%;"></td>
+                                                            <td style="width: 3ch;"></td>
+                                                            <td style="text-align: center; width: 3ch; padding-left: 0.75ch;">
+                                                                <span style="color: #D75A4A; font-size: 30px; font-weight: bolder;"><?php echo $info['likeCount'] ?></span>
                                                             </td>
                                                         </tr>
                                                         <tr>
-                                                            <td>
+                                                            <td style="width: 100%;"></td>
+                                                            <td style="width: 3ch;">
                                                                 <img src="assets/comment.png" id="comment_post_<?php echo $info['id'] ?>" onclick="openComment('comment_input_<?php echo $info['id'] ?>')" class="" style="border-width: 0; height: 3ch; margin-top: 0; cursor: pointer;" />
                                                             </td>
-                                                            <td>
+                                                            <td style="width: 3ch;">
                                                                 <img src=<?php if ($info['liked']) {echo "assets/heart-on.png";} else{echo "assets/heart-off.png";}?>  id="like_post_<?php echo $info['id'] ?>" onclick="toggleLike('<?php echo $info['id'] ?>', '<?php echo $_SESSION['username']?>')" class="" style="border-width: 0; height: 3ch; margin-left: 0.75ch; cursor: pointer;" />
                                                             </td>
                                                         </tr>
