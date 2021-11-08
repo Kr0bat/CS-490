2f3f120a41c34c037e62edc146a000c856f0ab57
m1nn1man <maxecohen2001@gmail.com>
2021-11-07 20:20:23 -0500

    adding like count in posts on feed

--- content-feed.php
+++ content-feed.php
@@ -431,8 +431,25 @@ if (isset($_GET['successMsg'])) {
                                         </div>
                                         <div class="col-6" style="height: 8ch; overflow: hidden; text-overflow: ellipsis; word-break: break-word; padding-top: 5ch; text-align: right;">
                                             <div class="col-12" style="">
-                                                <img src="assets/comment.png" id="comment_post_<?php echo $info['id'] ?>" onclick="openComment('comment_input_<?php echo $info['id'] ?>')" class="" style="border-width: 0; height: 3ch; margin-top: 0; cursor: pointer;" />
-                                                <img src=<?php if ($info['liked']) {echo "assets/heart-on.png";} else{echo "assets/heart-off.png";}?>  id="like_post_<?php echo $info['id'] ?>" onclick="toggleLike('<?php echo $info['id'] ?>', '<?php echo $_SESSION['username']?>')" class="" style="border-width: 0; height: 3ch; margin-left: 0.75ch; cursor: pointer;" />
+                                                <table style="width: 100%">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>
+                                                            </td>
+                                                            <td style="text-align: center">
+                                                                <span style="color: red; font-size: 30px; font-weight: bolder;"><?php echo $info['likeCount'] ?></span>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>
+                                                                <img src="assets/comment.png" id="comment_post_<?php echo $info['id'] ?>" onclick="openComment('comment_input_<?php echo $info['id'] ?>')" class="" style="border-width: 0; height: 3ch; margin-top: 0; cursor: pointer;" />
+                                                            </td>
+                                                            <td>
+                                                                <img src=<?php if ($info['liked']) {echo "assets/heart-on.png";} else{echo "assets/heart-off.png";}?>  id="like_post_<?php echo $info['id'] ?>" onclick="toggleLike('<?php echo $info['id'] ?>', '<?php echo $_SESSION['username']?>')" class="" style="border-width: 0; height: 3ch; margin-left: 0.75ch; cursor: pointer;" />
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
                                             </div>
                                         </div>
                                     </td>
