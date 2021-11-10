52fa768f96516ddfbe4898b6ba35e23e014578d6
m1nn1man <maxecohen2001@gmail.com>
2021-11-10 14:51:27 -0500

    trying to fix delete post functionality

--- content-account.php
+++ content-account.php
@@ -232,7 +232,7 @@ if (isBlocked($_GET['viewing'])) {
     </div>
     <div class="col-12" style="margin-top: 0vh">
         <div class="col-10 push-1">
-            <div class="col-11 bodyBold">
+            <div class="col-12 bodyBold">
                 <table style="width: 100%;">
                     <tbody>
                         <tr style="height: 1.5ch;"></tr>
@@ -342,7 +342,7 @@ if (isBlocked($_GET['viewing'])) {
     <div class="col-12" style="margin-top: 0vh">
         <div class="col-10 push-1">
             <div class="col-12" style="margin: 2ch 0 1ch 0">
-                <div class="col-11 bodyBold statContainer">
+                <div class="col-12 bodyBold statContainer">
                     <table style="width: 100%;">
                         <tbody>
                             <tr style="height: 1ch;"></tr>
@@ -452,7 +452,7 @@ if (isBlocked($_GET['viewing'])) {
         ?>
 
             <div class="col-12" style="margin: 2ch 0 1ch 0">
-                <div class="col-11 titleBold" id="post_container_<?php echo $info[id]?>" style="">
+                <div class="col-12 titleBold" id="post_container_<?php echo $info[id]?>" style="">
                     <div class="col-12 bodyBold postContainer" style="margin: 0">
                         <div class="col-12">
                             <table style="margin: 0;">
