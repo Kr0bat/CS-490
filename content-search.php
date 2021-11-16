337cc96e2147517422fe91e8f487a3f96bb7a40a
m1nn1man <maxecohen2001@gmail.com>
2021-11-16 13:56:41 -0500

    adding follow button on mobile search

--- content-search.php
+++ content-search.php
@@ -398,6 +398,7 @@ function closeDeleteConfirm(postID) {
 
                                         <?php 
                                             }
+                                        }
                                         ?>
 
                                     </tr>
